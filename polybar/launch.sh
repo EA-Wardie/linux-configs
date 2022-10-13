f97c2b5cf49e08bd97a86d150ce36cca9403f0d0
Edward van Tonder <56725833+EA-Wardie@users.noreply.github.com>
2022-10-13 21:35:45 +0200

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,7 +1,4 @@
  #!/bin/sh
 
-# Terminate already running bar instances
 killall -q polybar
-
-# Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar main &
+polybar --reload main & polybar --reload main2 &
